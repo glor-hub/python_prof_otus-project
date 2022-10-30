fe51f22e3bb5523ca84775a6e40f1f40e54c435e
Inna Sorina <sorina-inna@yandex.ru>
2022-10-31 01:21:24 +0300

    add handling community data from vk api.

--- vksearch/vkgroup/vkapi_services/community.py
+++ vksearch/vkgroup/vkapi_services/community.py
@@ -1,6 +1,8 @@
 from . import vkapiclient
 from .base_task import BaseTask
 
+import logging
+
 class CommunityTask(BaseTask):
     IDS_PER_TASK = 5
     URL_PATTERN = (
@@ -9,14 +11,31 @@ class CommunityTask(BaseTask):
         'v={version}&access_token={token}')
 
     def __init__(self):
+        self.response = None
         self.num = self.IDS_PER_TASK
 
     def build_url(self, ids, version, token):
         return self.URL_PATTERN.format(ids=ids, version=version, token=token)
 
     async def vk_get_communities(self, session, token):
-        ids = [id for id in range(1, (self.IDS_PER_TASK + 1))]
+        ids = [id for id in range(0, (self.IDS_PER_TASK))]
         version = vkapiclient.VERSION
-        url = self.build_url(ids, version,token)
-        data = await super().get_data(session,url)
-        print(data)
+        url = self.build_url(ids, version, token)
+        self.response = await super().get_data(session, url)
+        self.handle_response(self.response)
+
+    def handle_error(self):
+        errmsg = 'Unknown  error occurred'
+        err = self.response.get('error')
+        if err:
+            errmsg = err.get('error_msg', errmsg)
+            logging.error(errmsg)
+            raise Exception
+
+    def handle_response(self,resp):
+        data_list = resp.get('response')
+        if data_list:
+            logging.info(f'data received')
+            print(data_list)
+        else:
+            self.handle_error()
