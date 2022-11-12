c09cc68faaac026ef7978a9aaf58db468ea67a36
Inna Sorina <sorina-inna@yandex.ru>
2022-11-12 18:59:58 +0300

    11 groups with all required fields loaded from vk api. Used docker-compose.

--- vksearch/vkgroup/models.py
+++ vksearch/vkgroup/models.py
@@ -1,36 +1,59 @@
 from django.db import models
 
+
 class CommunityType(models.Model):
-    name = models.TextField(unique=True)
+    GROUPS_TYPES = [
+        ('GROUP', 'group'),
+        ('PAGE', 'page'),
+        ('EVENT', 'event')
+    ]
+    name = models.TextField(choices=GROUPS_TYPES, default='GROUP',unique=True)
     objects = models.Manager()
 
+
 class Community(models.Model):
-    AGES = [
+    AGE_UNKNOWN = 1
+    AGE_16_OLDER = 2
+    AGE_18_OLDER = 3
+    AGE_VK_TYPES = [
         (1, '0+'),
         (2, '16+'),
         (3, '18+')
-        ]
+    ]
     vk_id = models.PositiveIntegerField(primary_key=True, null=False)
     type = models.ForeignKey('CommunityType', on_delete=models.CASCADE, null=True)
     deactivated = models.BooleanField(default=False)
-    age_limits=models.PositiveIntegerField(choices=AGES, default=1,unique=True )
+    age_vk = models.PositiveIntegerField(choices=AGE_VK_TYPES, null=True)
     description = models.TextField(default='', null=True)
     verified = models.BooleanField(null=True)
     name = models.CharField(max_length=64)
-    site = models.CharField(max_length=128)
-    members = models.PositiveIntegerField(blank=True, default=0)
-    status = models.CharField(max_length=64)
+    site = models.CharField(max_length=128, null=True)
+    members = models.PositiveIntegerField(blank=True,null=True)
+    status = models.CharField(max_length=64, null=True)
+    update=models.DateTimeField(auto_created=False, auto_now_add=True)
 
     objects = models.Manager()
+
     # def save(self, *args, **kwargs):
     #     post_data = {'remote_api_file_field': self.file}
     #     get_data_from_vk()
     #     requests.post(REMOTE_API_URL, data=post_data)
     #     super(Community).save()
     def __str__(self):
-        return (f'community: {self.name} id: {vk_id}')
+        return (f'community: {self.name} id: {self.vk_id}')
 
-class Age(models.Model):
+
+class AgeRange(models.Model):
+    AGE_UNKNOWN = 1
+    AGE_16_18 = 2
+    AGE_18_24 = 3
+    AGE_24_30 = 4
+    AGE_30_35 = 5
+    AGE_35_45 = 6
+    AGE_45_55 = 7
+    AGE_55_65 = 8
+    AGE_65_OLDER = 9
+
     AGES_RANGE = [
         (1, 'ALL'),
         (2, '16-18'),
@@ -42,29 +65,42 @@ class Age(models.Model):
         (8, '55-65'),
         (9, '65+')
     ]
-    range = models.PositiveIntegerField(choices=AGES_RANGE, default=1, unique=True)
+    range = models.PositiveIntegerField(choices=AGES_RANGE, default=AGE_UNKNOWN, unique=True)
 
     def __str__(self):
-        return f('age range: {self.range}')
+        return f'age range: {self.range}'
 
 
 class Country(models.Model):
     name = models.TextField(unique=True)
     objects = models.Manager()
-# method: database.getCountriesById
+
+    # method: database.getCountriesById
     def __str__(self):
         return self.name
 
+
 class AudienceProfile(models.Model):
+    SEX_UNKNOWN = 0
+    SEX_FEMALE = 1
+    SEX_MALE = 2
+    SEX_CHOICES = (
+        (SEX_UNKNOWN, 'UNKNOWN'),
+        (SEX_FEMALE, 'Female'),
+        (SEX_MALE, 'Male')
+    )
     country = models.ForeignKey('Country', on_delete=models.CASCADE, null=True)
-    users_age=models.ForeignKey('Age', on_delete=models.CASCADE, null=True)
+    age_range = models.ForeignKey('AgeRange', on_delete=models.CASCADE, null=True)
+    sex = models.SmallIntegerField(choices=SEX_CHOICES, default=SEX_UNKNOWN)
+
     class Meta:
-        unique_together = (('country', 'question'),)
+        unique_together = ('country', 'age_range', 'sex')
 
 
 class Audience(models.Model):
-    community = models.ForeignKey(Community,  on_delete=models.CASCADE, null=True)
-    profile = models.ForeignKey(AudienceProfile,  on_delete=models.CASCADE, null=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, null=True)
+    profile = models.ForeignKey(AudienceProfile, on_delete=models.CASCADE, null=True)
     count = models.IntegerField()
+
     def __str__(self):
-        return (f'audience id:{pk} of {community.name} ')
+        return (f'audience id:{self.pk} of community {self.community.name} ')
--- vksearch/vkgroup/tasks.py
+++ vksearch/vkgroup/tasks.py
@@ -3,7 +3,6 @@ import requests
 from celery import shared_task
 from datetime import datetime
 
-
 from . import vkapiclient
 from .models import Community, CommunityType
 
@@ -37,12 +36,15 @@ def task_vk_get_data_communities(ids, version, token):
     comm_data_list = r.json().get('response')
     return comm_data_list
 
+
 def parse_data_communities():
     pass
+
 
 @shared_task()
 def task_write_data_communities_to_db():
     pass
+
 
 def process_data_communities():
     pass
@@ -58,7 +60,7 @@ def test_create_task():
         print(data_list)
         types = ['group', 'page', 'event']
         for type in types:
-            comm_type = CommunityType.objects.get_or_create(
+            comm_type,_ = CommunityType.objects.get_or_create(
                 name=type,
             )
             print(comm_type)
@@ -66,27 +68,41 @@ def test_create_task():
             # count += 1
             # if data.get('deactivated') or not data.get('members'):
             #     continue
-            vk_id = int(data.get('id'))
             dtype = data.get('type')
-            # deactivated = int(data.get('deactivated'))
-            description = data.get('description')
-            # verified = int(data.get('verified'))
-            print(vk_id)
-            comm_type = CommunityType.objects.get(
+
+            comm_type,_ = CommunityType.objects.get_or_create(
                 name=dtype
             )
-            comm = Community.objects.get_or_create(
-                vk_id=vk_id,
-                # deactivated=deactivated,
-                description=description
-                # verified=verified
-            )
-            comm = Community.objects.get(
-                vk_id=vk_id
-                      )
-            comm.type = comm_type
+            params = {
+                'vk_id': data.get('id'),
+                'deactivated': bool(data.get('deactivated')),
+                'description': data.get('description'),
+                'verified': data.get('verified'),
+                'age_vk': data.get('age_limits'),
+                'name': data.get('name'),
+                'site': data.get('site'),
+                'members': data.get('members_count'),
+                'status': data.get('status')
+            }
+            # vk_id = int(data.get('id'))
+            # deactivated = int(data.get('deactivated'))
+            # description = data.get('description')
+            #
+            # print(vk_id)
 
-            comm.save()
+            comm,created = Community.objects.get_or_create(
+                **params
+                # vk_id=vk_id,
+                # # deactivated=deactivated,
+                # description=description
+                # # verified=verified
+            )
+            # comm = Community.objects.get(
+            #     vk_id=vk_id
+            # )
+            if created:
+                comm.type = comm_type
+                comm.save()
             print(comm)
         # print(r)
         return {"status": True}
