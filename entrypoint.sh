29dbff814aa2b74d07a3ee573b1ba475134c0302
Inna Sorina <sorina-inna@yandex.ru>
2022-11-24 00:45:24 +0300

    update main.yml.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,12 @@
 #!/bin/bash
 set -xeu
-#make --quiet --directory="$HOME/celery" clean-pyc
 
 echo "Apply migrations"
-#if [ "$DATABASE" = "postgres" ]
-#then
-#    echo "Waiting for postgres..."
-#
-#    while ! nc -z $SQL_HOST $SQL_PORT; do
-#      sleep 0.1
-#    done
-#
-#    echo "PostgreSQL started"
-#fi
+
 
 
 python manage.py migrate
 python manage.py collectstatic --no-input
 
-#python vksearch/manage.py migrate
-#python manage.py collectstatic --no-input --clear
-
 exec "$@"
 
-#!/usr/bin/env bash
-
-#set -e
-#
-#echo "Apply migrations"
-#
-#flask db upgrade
-#
-#echo "migrations ok"
-#
-#exec "$@"
-
-# prestart.sh echo hello world
-
-# Apply migrations
-# upgrade..
-# migrations ok
-# hello world
